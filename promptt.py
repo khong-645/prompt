d932603b62dc147eb8da40381ccb14e7e2135a90
khong-645 <16632021@aru.ac.th>
2025-05-29 14:12:32 +0700

    Update promptt.py

--- promptt.py
+++ promptt.py
@@ -8,7 +8,7 @@ st.title("แสดงภาพจาก URL ที่เลือก")
 # รายการ URL ของภาพ
 image_urls = {
     "Bulldog": "https://upload.wikimedia.org/wikipedia/commons/b/bf/Bulldog_inglese.jpg",
-    "Tyrannosaurus": "https://upload.wikimedia.org/wikipedia/commons/thumb/4/4d/SUE-T-REX-FMNH-PR-2081-By-JJxFile.jpg/330px-SUE-T-REX-FMNH-PR-2081-By-JJxFile.jpg",
+    "brooch": "https://upload-os-bbs.hoyolab.com/upload/2023/02/07/5774947/8105b30243238bce2ef7c8a35934bd6f_9170432348253218955.png?x-oss-process=image%2Fresize%2Cs_1000%2Fauto-orient%2C0%2Finterlace%2C1%2Fformat%2Cwebp%2Fquality%2Cq_70.jpg",
     "Kiana": "https://upload-os-bbs.hoyolab.com/upload/2023/02/07/5774947/713cbe914f7c32a3e4364e426105591c_8715800185506906620.png?x-oss-process=image%2Fresize%2Cs_1000%2Fauto-orient%2C0%2Finterlace%2C1%2Fformat%2Cwebp%2Fquality%2Cq_70.jpg"
 }
 
