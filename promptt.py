b091712f54c78c8c4e174ffb2fbbc7c06ab3a8a0
khong-645 <16632021@aru.ac.th>
2025-05-29 14:06:23 +0700

    Update promptt.py

--- promptt.py
+++ promptt.py
@@ -17,6 +17,6 @@ try:
     response = requests.get(image_url)
     response.raise_for_status()  # ตรวจสอบว่าโหลดสำเร็จ
     img = Image.open(BytesIO(response.content))
-    st.image(img, caption="ภาพจาก Wikipedia", use_column_width=True)
+    st.image(img, caption="ภาพจาก Wikipedia", use_container_width=True)
 except Exception as e:
     st.error(f"ไม่สามารถโหลดภาพได้: {e}")
